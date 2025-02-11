e710af89553d69d15eddedbe705bd8520c14b1fa
paologhidoni <paolo.ghidoni82@gmail.com>
2025-02-11 15:33:54 +0100

    Setup environment for tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/tictactoe/",
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: true,
+    setupFiles: "./src/tests/setup.ts",
+  },
 });
